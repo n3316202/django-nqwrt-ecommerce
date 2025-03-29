6e572ac92453593401d0393aeda1a4276ed54b0e
heo <admin@admin.com>
2025-03-30 00:16:45 +0900

    commit

--- store/views.py
+++ store/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from store.models import Product
+from store.models import Category, Product
+from django.contrib import messages
+from django.shortcuts import redirect, render
 
 
 # Create your views here.
@@ -20,3 +23,40 @@ def about(request):
 def product(request, product_id):
     product = Product.objects.get(id=product_id)
     return render(request, "store/product.html", {"product": product})
+
+
+# dev_14
+def category(request, foo):
+    # Replace Hyphens with Spaces
+
+    foo = foo.replace("-", " ")
+    # print(foo)
+
+    # Grab the category from the url
+    try:
+        # Look up the category
+        category = Category.objects.get(name=foo)
+        print(category)
+
+        products = Product.objects.filter(category=category)
+        print(products)
+
+        return render(
+            request,
+            "store/category.html",
+            {
+                "products": products,
+                "category": category,
+                "categories": Category.objects.all,
+            },
+        )
+
+    except:
+        messages.success(request, ("카테고리가 존재하지 않습니다."))
+        return redirect("store:home")
+
+
+# dev_14
+def category_summary(request):
+    categories = Category.objects.all()
+    return render(request, "store/category_summary.html", {"categories": categories})
