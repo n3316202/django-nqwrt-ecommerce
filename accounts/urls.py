443840c1dbaec4aabd454468d4622cd03e3598af
heo <admin@admin.com>
2025-03-29 15:42:12 +0900

    commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,14 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from accounts import views
 
 # dev_8
 app_name = "accounts"
 
 urlpatterns = [
-    # django.contrib.auth앱의 LoginView 클래스를 활용했으므로 별도의 views.py 파일 수정이 필요 없음
-    # path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path("login/", views.login_user, name="login_user"),
-    path("logout/", views.logout_user, name="logout_user"),
-    path("register/", views.register_user, name="register_user"),
+    # path("logout/", views.logout_user, name="logout_user"),
+    # path("register/", views.register_user, name="register_user"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 # dev_8
@@ -9,3 +10,22 @@ from django.contrib.auth.models import User
 def logout_user(request):
     logout(request)
     return redirect("/")
+
+
+# dev_9
+def login_user(request):
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You Have been logged in")
+            return redirect("/")
+        else:
+            messages.success(request, ("There was an error, please try again"))
+            return redirect("login")
+    else:
+        return render(request, "accounts/login.html", {})
