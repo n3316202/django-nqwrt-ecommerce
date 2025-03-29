b861addbab1534174f25271b065f3c938a048d64
heo <admin@admin.com>
2025-03-29 21:32:25 +0900

    commit

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,5 +4,43 @@ from accounts.models import User
 # Register your models here.
 
 
-# 기본적인 관리자 페이지에서 Question을 등록
-admin.site.register(User)
+# 1. 기본적인 관리자 페이지에서 기본적인 등록
+# admin.site.register(User)
+
+
+# 2.커스텀 모델을 등록하는 방법을 알았으니, 커스터마이징 옵션에 대해 알아보도록 하겠습니다.
+class UserAccountsAdmin(admin.ModelAdmin):
+    list_display = [
+        "username",
+        "email",
+        "job",
+        "gender",
+    ]
+
+    # list_display = [
+    #     "username",
+    #     "email",
+    #     "job",
+    #     "gender",
+    # ]
+
+    # 커스터마이징 코드
+    # 링크걸기
+    list_display_links = [
+        "username",
+        "email",
+        "job",
+        "gender",
+    ]
+
+    list_filter = (
+        "username",
+        "email",
+        "job",
+        "gender",
+    )
+
+    search_fields = ["username", "email", "job"]
+
+
+admin.site.register(User, UserAccountsAdmin)
