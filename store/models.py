02d88147f7ea5f0178320b993ce6845ae1539dac
heo <admin@admin.com>
2025-03-30 21:14:51 +0900

    commit

--- store/models.py
+++ store/models.py
@@ -18,7 +18,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     # dev_6
     is_sale = models.BooleanField(default=False)
-    sale_price = models.IntegerField(default=0)
+    sale_price = models.IntegerField(default=0, blank=True, null=True)
 
     def __str__(self):
         return self.name
