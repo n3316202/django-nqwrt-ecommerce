1f17fd91d9510db1db71107788ec0b2a224fe2a1
heo <admin@admin.com>
2025-04-02 17:49:39 +0900

    commit

--- payment/models.py
+++ payment/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from orders.models import Order
 
 
+# dev_26
 # Create your models here.
 class Payment(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
--- payment/views.py
+++ payment/views.py
@@ -12,10 +12,17 @@ from django.contrib.auth.decorators import login_required
 from payment.models import Payment
 from django.contrib import messages
 
+from cart.cart import Cart
+from django.contrib import messages
+from store.models import Product
+
+from orders.forms import ShippingForm
+from orders.models import Order, OrderItem, ShippingAddress
+
 
 # dev_26
 # Create your views here.
-@login_required
+@login_required(login_url="accounts:login_user")
 def payment_process(request):
 
     # 결제 성공 시 로직
@@ -26,42 +33,52 @@ def payment_process(request):
 
         cart = Cart(request)
 
-        # 카트 데이타 프레임 가져오기
-        df_cart = cart.get_data_frame_products()
-
         # if total_price == int(request.POST['paid_amount']): #테스트를 위하여 10으로 넣고 대입
         if 100 == int(request.POST["paid_amount"]):
+
             # logged in
             user = request.user
 
-            # create order
+            # Create Order
             create_order = Order(user=user)
-            create_order.amount_paid = df_cart["sum_price"].sum()
+            create_order.amount_paid = cart.get_product_total()  # 총액
             create_order.save()
 
             # Add Order Items
             # Get the oorder ID
             order_id = create_order.pk
 
-            for index, row in df_cart.iterrows():
+            # {'quantity': 1, 'price': Decimal('10000.00'), 'product': <Product: 너를위한-장고>, 'total_price': Decimal('10000.00')}
+            for item in cart:
+                print(item)
                 # Create Order Item
                 create_order_item = OrderItem(
                     order_id=order_id,
-                    product_id=row["id"],
-                    quantity=row["quantity"],
-                    price=row["final_price"],
+                    product_id=item["product"].id,
+                    quantity=item["quantity"],
+                    price=item["price"],
                 )
                 create_order_item.save()
+
+            form = ShippingForm(request.POST)
+
+            if form.is_valid():
+                shipping = form.save(commit=False)  # 저장은 하지 않고 객체만 생성
+                shipping.user = (
+                    request.user
+                )  # ForeignKey 값 추가 (현재 로그인한 사용자)
+                shipping.save()  # 최종적으로 저장
 
             # 결재 데이터 저장
             create_payment = Payment(order=create_order)
             create_payment.imp_uid = request.POST["imp_uid"]
             create_payment.save()
 
-            # 카트를 담고 있는 세션 지우기
             # Delete cart item(만약 카트도 지우고 싶다면)
-            for key in list(cart.cart.keys()):
-                cart.delete(key)
+            cart_keys = list(cart.get_dic_cart().keys())
+            for product_id in cart_keys:
+                product = Product.objects.get(id=product_id)
+                cart.remove(product)
 
             messages.success(request, "결재가 완료 되었습니다.")
             return HttpResponse("SUCCESS")
