844d76db2c8c8ae6d1581dc7b3e2ec9ab368013e
heo <admin@admin.com>
2025-04-04 17:38:38 +0900

    commit

--- api/views/product_views.py
+++ api/views/product_views.py
@@ -10,7 +10,6 @@ from api.serializers import ProductSerializer
 # Create your views here.
 
 
-
 # dev_29 추가 되도록
 @api_view(["GET", "POST"])
 def products_api(request):
@@ -26,3 +25,22 @@ def products_api(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def product_api(request, pk):
+    product = get_object_or_404(Product, id=pk)
+
+    if request.method == "GET":
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    elif request.method == "PUT":
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    elif request.method == "DELETE":
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
