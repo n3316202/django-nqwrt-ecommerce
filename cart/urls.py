e4d05613d60c2cfeaa89b16311614a6a6ec8e783
heo <admin@admin.com>
2025-04-07 11:20:08 +0900

    commit

--- cart/urls.py
+++ cart/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path("", views.cart_summary, name="cart_summary"),  # dev_18
     path("add/", views.cart_add, name="cart_add"),
     path("delete/", views.cart_delete, name="cart_delete"),  # dev_19
-    path("update/", views.cart_update, name="cart_update"),  # dev_19
+    path("update/", views.cart_update, name="cart_update"),  # dev_20
 ]
