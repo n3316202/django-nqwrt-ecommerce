0b58400e19f38454c5e6c3b991997879c6a2a120
heo <admin@admin.com>
2025-04-12 12:38:14 +0900

    commit

--- config/settings.py
+++ config/settings.py
@@ -209,7 +209,7 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),  # timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
