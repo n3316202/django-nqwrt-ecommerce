aa609b46c671deff83fcf900012c88e85fecb4fe
heo <admin@admin.com>
2025-04-13 18:31:33 +0900

    commit

--- accounts/adapters.py
+++ accounts/adapters.py
@@ -2,6 +2,8 @@ from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 
 
 class KakaoSocialAccountAdapter(DefaultSocialAccountAdapter):
+    # 소셜 로그인 유저 객체를 초기화
+    # User 모델 필드를 채워주는 메서드
     def populate_user(self, request, sociallogin, data):
         user = super().populate_user(request, sociallogin, data)
         kakao_data = sociallogin.account.extra_data
@@ -23,4 +25,44 @@ class KakaoSocialAccountAdapter(DefaultSocialAccountAdapter):
         if not user.job:
             user.job = "E"
 
+        return user
+
+    # dev_41
+    # DB에 저장되는 과정
+
+    def save_user(self, request, sociallogin, form=None):
+        """
+        Saves a newly signed up social login user (Kakao).
+        """
+        user = sociallogin.user
+
+        print(user)
+        # --- Kakao-specific data 처리 ---
+        kakao_data = sociallogin.account.extra_data
+        kakao_account = kakao_data.get("kakao_account", {})
+        profile = kakao_account.get("profile", {})
+
+        # 이메일
+        user.email = kakao_account.get("email", "")
+
+        # 프로필 이미지
+        user.profile_image = profile.get("profile_image_url")
+
+        # 성별
+        kakao_gender = kakao_account.get("gender")
+        if kakao_gender == "male":
+            user.gender = "M"
+        elif kakao_gender == "female":
+            user.gender = "F"
+
+        # 직업 기본값 설정
+        if not user.job:
+            user.job = "E"  # 기타
+
+        if not user.password:
+            user.set_password(user.email)
+
+        #user.save()
+        sociallogin.save(request)
+        
         return user
