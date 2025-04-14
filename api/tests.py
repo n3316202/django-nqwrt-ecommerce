c81bcbd402a85a24d3253717c67aa45e9d073050
heo <admin@admin.com>
2025-04-14 23:24:33 +0900

    commit

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+def add(num1, num2):
+    return num1 + num2
+
+
+def minus(num1, num2):
+    return num1 - num2
+
+
+dic_calculator = {
+    "add/": add,
+    "minus/": minus,
+}
+
+
+class AnyTest(TestCase):
+    def setUp(self):
+        pass
+
+    def test_calculator(self):
+        url = "add/"
+        print(url)
+        print(dic_calculator[url](3, 2))
