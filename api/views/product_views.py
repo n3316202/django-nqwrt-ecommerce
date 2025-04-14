7aa64210fc354bf56db1f5eee9b7b87d2607f46b
heo <admin@admin.com>
2025-04-14 15:44:12 +0900

    commit

--- api/views/product_views.py
+++ api/views/product_views.py
@@ -16,6 +16,8 @@ def products_api(request):
 
     if request.method == "GET":
         products = Product.objects.all()
+        # many=True ➜ 여러 개의 인스턴스 (QuerySet, 리스트 등)
+        # many=False (기본값) ➜ 단일 인스턴스
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
 
